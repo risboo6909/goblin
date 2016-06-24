0ce1a3300fcbe34b9d7bd780d085ca028bc9202f
risboo6909 <ttyv00@gmail.com>
2016-06-24 18:48:45 +0300

    Generate session ids

--- goblin-misc/manager.go
+++ goblin-misc/manager.go
@@ -5,13 +5,47 @@ import (
 	"math/rand"
 )
 
+
 // Sessions manager
 
 type Session struct {
 	Board *BoardDescription
 	AI AIOptions
+	SessionID string
 }
 
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+// generate random ID n symbols length, taken from
+// http://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-golang
+func generateSessionId(n int) string {
+
+	var src = rand.NewSource(time.Now().UnixNano())
+
+	// session id will be just a time it was created
+	b := make([]byte, n)
+
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
 
 func CreateNewSession(boardSide int, player Cell) Session {
 
@@ -23,6 +57,8 @@ func CreateNewSession(boardSide int, player Cell) Session {
 		AIOptions{switchPlayer(player),
 			  5,
 		          4},
+
+		generateSessionId(10),
 	}
 }
 
