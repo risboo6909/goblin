64d0e0897d2b72fda4b029382fa24c7bffd64569
risboo6909 <ttyv00@gmail.com>
2016-06-26 03:26:40 +0300

    Add intervals unfolder

--- misc/structs.go
+++ misc/structs.go
@@ -1,10 +1,9 @@
 package misc
 
-
 type ScanDirection uint8
 
-type Move struct {
-	col, row int
+type CellPosition struct {
+	Col, Row int
 }
 
 // Interval represents indexes of start and end of an n-length chain`
@@ -12,8 +11,45 @@ type Interval struct {
 
 	Direction  ScanDirection
 
-	Col1, Row1 int
-	Col2, Row2 int
+	From CellPosition
+	To CellPosition
+
+}
+
+// Unfolds interval encoded by Interval data structure
+func (interval Interval) Unfold() []CellPosition {
+
+	result := []CellPosition{}
+
+
+	switch interval.Direction {
+	case LRDiagonal:
+		result = make([]CellPosition, interval.To.Col - interval.From.Col + 1)
+		for idx := 0; idx < interval.To.Col - interval.From.Col + 1; idx++ {
+			result[idx] = CellPosition{interval.From.Col + idx, interval.From.Row + idx}
+		}
+		break
+	case RLDiagonal:
+		result = make([]CellPosition, interval.From.Col - interval.To.Col + 1)
+		for idx := 0; idx < interval.From.Col - interval.To.Col + 1; idx++ {
+			result[idx] = CellPosition{interval.From.Col - idx, interval.From.Row + idx}
+		}
+		break
+	case horizontal:
+		result = make([]CellPosition, interval.To.Col - interval.From.Col + 1)
+		for idx := 0; idx < interval.To.Col - interval.From.Col + 1; idx++ {
+			result[idx] = CellPosition{interval.From.Col + idx, interval.From.Row}
+		}
+		break
+	case vertical:
+		result = make([]CellPosition, interval.To.Row - interval.From.Row + 1)
+		for idx := 0; idx < interval.To.Row - interval.From.Row + 1; idx++ {
+			result[idx] = CellPosition{interval.From.Col, interval.To.Row + idx}
+		}
+		break
+	}
+
+	return result
 
 }
 
